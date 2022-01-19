11fd01ca24e355ff459dd39cb7e5d28ba33f76a9
Nicolas Lemoine <nico.lemoine@gmail.com>
2022-01-19 18:03:33 +0100

    up

--- src/Twig/ImageFactoryRuntime.php
+++ src/Twig/ImageFactoryRuntime.php
@@ -49,4 +49,133 @@ class ImageFactoryRuntime implements RuntimeExtensionInterface
 
         return $image;
     }
+
+    public function apply($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function background($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function blur($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function border($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function brightness($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function contrast($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function crop($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function device_pixel_ratio($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function fit($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function flip($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function focal_crop($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function gamma($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function greyscale($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function height($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function manipulate($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function manual_crop($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function optimize($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function orientation($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function pixelate($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function quality($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function sepia($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function sharpen($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function to($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function watermark($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function watermark_fit($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function watermark_height($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function watermark_opacity($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function watermark_padding($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function watermark_position($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function watermark_width($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function width($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function widths($source, array $args = []) {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
 }
